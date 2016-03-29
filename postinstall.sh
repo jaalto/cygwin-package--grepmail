49f0d900effff3dc1acdaa0b7a989be3af3bee48
Jari Aalto <jari.aalto@cante.net>
2016-03-29 10:30:53 +0300

    postinstall.sh: update script (automatic)

--- postinstall.sh
+++ postinstall.sh
@@ -3,3 +3,23 @@
 from="/usr/share/perl/cygwin-pods/grepmail.pod"
 to="/usr/lib/perl5/5.8/cygwin/perllocal.pod"
 cat "$from" >> "$to"
+#:etc
+fromdir=/etc/defaults
+for i in  etc/
+do
+    from="$fromdir/$i"
+    to="$dest/$i"
+
+    [ -e "$from" ] || continue
+    [ -e "$to"   ] && continue
+
+    case "$i" in
+        */) # Directory
+            install -d -m 755 "$to"
+            ;;
+        *)  # File
+            install -v -m 644 "$from" "$to"
+            ;;
+    esac
+done
+
